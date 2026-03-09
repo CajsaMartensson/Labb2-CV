d0a484c1f0b672abe8a8ec4470dc6ea4c7409f27
CajsaMartensson <cajsa.maartensson@gmail.com>
2026-03-09 14:55:20 +0100

    Test css fix

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ async function GetData(){
     if(!response.ok){
         throw new Error("Kunde inte hämta data: " + response.statusText);
     }
-    const data = await response.json();
+    const {jobs, education, skills} = await response.json();
     console.log(data);
-    showJobExperience(data.jobs);
-    showEducation(data.education);
-    showSkills(data.skills);
+    showJobExperience(jobs);
+    showEducation(education);
+    showSkills(skills);
 }
 
 GetData();
